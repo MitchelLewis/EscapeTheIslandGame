aa179b704f998508126399ca7636ac1c20128784
Lewis Mitchell <52708269+MitchelLewis@users.noreply.github.com>
2021-10-14 16:08:19 +0100

    ++Hint number updates after using command (#2)
    Co-authored-by: Harry <33515629+Morbid-Harry@users.noreply.github.com>

--- src/application/commands/HintCommand.java
+++ src/application/commands/HintCommand.java
@@ -1,7 +1,10 @@
 package application.commands;
 
 import application.LevelController;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.Alert.AlertType;
 
 public class HintCommand implements Command {
 
@@ -14,7 +17,38 @@ public class HintCommand implements Command {
 	@Override
 	public void handleCommand(TextArea gameOutput, String input) {
 		String hintText = levelController.getRiddle().getHint();
-		gameOutput.appendText("\n" + hintText);
+		
+		//get scene from text area then lookup the label from the scene
+		Label hintAmountLbl = (Label) gameOutput.getScene().lookup("#hintAmount");
+		
+		//Value of label text converted to int
+		int hintValue = Integer.parseInt(hintAmountLbl.getText());
+		
+		//Check hint for this question has not already been used
+		if(levelController.getRiddle().getUsedHint() == false)
+		{
+			//check the value is not 0 else alert they have not hints left
+			if(hintValue != 0)
+			{
+				gameOutput.appendText("\n" + hintText);
+				//update hint label
+				hintAmountLbl.setText(Integer.toString(hintValue - 1));
+				levelController.getRiddle().setUsedHint(true);
+			}
+			else
+			{
+				Alert alert = new Alert(AlertType.INFORMATION, "You have no hints left!");
+				alert.showAndWait();
+			}
+		}
+		else 
+		{
+			Alert alert = new Alert(AlertType.INFORMATION, "Only 1 hint for each riddle!");
+			alert.showAndWait();
+		}
+
+
+		
 	}
 
 }
