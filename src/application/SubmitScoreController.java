33e40a50e3579e1624d93239e8160cfa22f9cc61
Lewis Mitchell <52708269+MitchelLewis@users.noreply.github.com>
2021-11-11 17:18:42 +0000

    Added POSTing to website to submit score

--- src/application/SubmitScoreController.java
+++ src/application/SubmitScoreController.java
@@ -3,8 +3,14 @@ package application;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpClient.Version;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
@@ -21,20 +27,33 @@ public class SubmitScoreController {
 	
 	@FXML
 	public void initialize() {
-		this.httpClient = HttpClient.newHttpClient();
+		this.httpClient = HttpClient.newBuilder().version(Version.HTTP_1_1).build();
 	}
 	
 	public void handleSubmitScore(MouseEvent event) {
 		String name = nameEntry.getText();
 		String score = scoreLabel.getText();
-		sendScoreToWebsite(name, score, nameEntry.getScene());
+		try {
+			sendScoreToWebsite(name, score, nameEntry.getScene());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	private void sendScoreToWebsite(String name, String score, Scene scene) {
-	    String body = "{\"name\": \"" + name + "\", \"score\": \"" + score + "\"}";
+	private void sendScoreToWebsite(String name, String score, Scene scene) throws JsonProcessingException {
+	    Map<String, String> body = new HashMap();
+	    body.put("name", name);
+	    body.put("score", score);
+	    ObjectMapper mapper = new ObjectMapper();
+	    String requestBody = mapper
+	            .writerWithDefaultPrettyPrinter()
+	            .writeValueAsString(body);
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://httpbin.org/post"))
-                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .uri(URI.create("https://escape-the-island-game.herokuapp.com/score/submit"))
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .setHeader("Content-Type", "application/json")
+                
                 .build();
         try {
 			HttpResponse<String> response = httpClient.send(request,
