4741c4af3cf62a87fabbd19615dff8125a3da8a8
Lewis Mitchell <52708269+MitchelLewis@users.noreply.github.com>
2021-12-04 16:40:25 +0000

    Added test cases + Junit

---
+++ src/test/java/FontSetterTest.java
@@ -0,0 +1,15 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FontSetterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
